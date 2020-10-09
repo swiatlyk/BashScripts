c44208e888b406ce8fe7a5fe04544e0bf597da35
swiatlyk <swiatlyk@gmail.com>
2020-10-09 14:41:35 +0200

    Added option to read branch name from .git/config file

--- github_updater.sh
+++ github_updater.sh
@@ -14,9 +14,15 @@ for i in $(find . -name ".git" | cut -c 3-); do
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
     cd ..;
+    echo "Reading branch name from config file"
+    cd .git
+    BRANCH=$(cat config | grep branch | cut -c 9- | rev | cut -c 2- | rev | cut -c 2- | rev | cut -c 2- | rev)
+    echo -e "Found branch: \e[31m"$BRANCH"\e[0m"
+    cd ..;
 
     # finally pull
-    git pull origin master;
+    echo -e "Pulling changes from branch \e[31m"$BRANCH"\e[0m"
+    git pull origin $BRANCH;
 
     # lets get back to the CUR_DIR
     cd $CUR_DIR
